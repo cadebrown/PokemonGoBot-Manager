5c30ef95b5eccd1ecb396c7cbef1678073334ff7
CadeBrown <brown.cade@gmail.com>
2016-08-01 19:37:16 -0400

    Added auth server support, + bug fixes + speed

--- start.py
+++ start.py
@@ -3,74 +3,77 @@ import subprocess
 import signal
 import sys
 import json
+import time
 
 from pprint import pprint
 
-#Formatting stuff
 bar="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 bre="\n\n"
 
-#Send with breaks
 def formatMsg(msg):
     print bar + bre +bre + msg + bre + bre + bar
 
-#Opening json
 with open("locations.json") as loc_file:
     loc_data = json.load(loc_file)
 
-#Open bot info
 with open("bots.json") as bot_file:
     bot_data = json.load(bot_file)
 
-#Intro
 formatMsg("You are running PokeFarm by Cade. v1.0\nLocation info given below")
 
-#Start building a string
 loc_fullmsg = "Locations:\n\n"
 
-#Read up to 20 locations
 for location in loc_data:
     t_loc = loc_data[location]
     loc_fullmsg = loc_fullmsg + "Index: " + t_loc["index"] + "\nName: " + t_loc["name"] + "\nLocation: " + t_loc["loc"] + "\nInfo: " + t_loc["info"] + bre
 
-#Print out all the location names
 formatMsg(loc_fullmsg)
 
-#Gets command for username and password
-def getCmd(un, pw):
-    return "until (python pokecli.py -u " + un + " -p " + pw + " -l " + loc + "); do echo 'Process crashed with exit code $?.  Respawning..' >&2; sleep 1000; done;"
+def getCmd(un, pw, auth):
+    #return "until (python pokecli.py -u " + un + " -p " + pw + " -l " + loc + "); do echo 'Process crashed with exit code $?.  Respawning..' >&2; done;"
+    return "python ./pokecli.py -u " + un + " -p " + pw + " -l " + loc + " -a " + auth
     #return "echo $PWD"
 
+def runBotN(n):
+    if all_processes[n] != None:
+        try:
+            os.kill(all_processes[n], 0)
+            return
+        except OSError:
+            if int(bots[n]["enabled"]) != 1:
+                return
+    cmd = getCmd(bots[n]["username"], bots[n]["password"], bots[n]["auth_service"])
+    print cmd
+    process = subprocess.Popen(cmd.split())
+    #process = os.system(cmd)    
+    all_processes[n] = process.pid
 
-#Collecintg PIDs
-all_processes = []
-
-#Determining where the user wants to farm
 loc_index = str(input("Location Index: "))
 for location in loc_data:
     if int(loc_data[location]["index"]) == int(loc_index):
         loc = loc_data[location]["loc"] 
         break
 
-#Loop through and create processes
+bots = []
 for bot in bot_data:
-    if int(bot_data[bot]["enabled"]) != 1:
-        continue
-    #We build the cmd string/arraym with autorestart
-    cmd = getCmd(bot_data[bot]["username"], bot_data[bot]["password"])
-    #Print out for debugging
-    print cmd
+    bots.append(bot_data[bot])
 
-    #Create it from our builder
-    process = subprocess.Popen(cmd, shell=True)
-    #Store it's pid
-    all_processes.append(process.pid)
+all_processes = [None] * len(bots)
 
-#it waits for them to press enter
-print bar + bre + "Press enter to close anytime" + bre + bar
-raw_input("Press to close")
+for m in range(0, len(bots)):
+    runBotN(m)
 
-for p in all_processes:
-    os.kill(p, signal.SIGINT)
-    print "Killed " + str(p)
-print bar + bre + "Bot is done" + bre + bar
+print all_processes
+print bar + bre + "Use CTRL-C to close" + bre + bar
+
+try:
+    while True:
+        for k in range(0, len(bots)):
+            runBotN(k)
+        time.sleep(30)
+except KeyboardInterrupt:
+    print "Now killing all bots"
+    for p in all_processes:
+        os.kill(int(p), signal.SIGINT)
+        print "Killed " + str(p)
+    print bar + bre + "Bot is done" + bre + bar
